bbaacba62ae0d8d79b2fc39e65aeeaa4989feff0
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-08-22 20:03:39 -0700

    three child appended

--- js/select.js
+++ js/select.js
@@ -1,7 +1,56 @@
 // console.log('from select .js')
+// no 1 
 document.getElementById('ozil-btn').addEventListener('click',function(){
     const list = document.getElementById('list');
     const name = document.getElementById('ozil');
+    const realName = name.innerText;
+    const li = document.createElement('li')
+    li.innerText = realName;
+    list.appendChild(li);
+    
+})
+// no 2
+document.getElementById('salah-btn').addEventListener('click',function(){
+    const list = document.getElementById('list');
+    const name = document.getElementById('salah');
+    const realName = name.innerText;
+    const li = document.createElement('li')
+    li.innerText = realName;
+    list.appendChild(li);
+    
+})
+// no 3 
+document.getElementById('karim-btn').addEventListener('click',function(){
+    const list = document.getElementById('list');
+    const name = document.getElementById('karim');
+    const realName = name.innerText;
+    const li = document.createElement('li')
+    li.innerText = realName;
+    list.appendChild(li);
+    
+})
+// no 4
+document.getElementById('karim-btn').addEventListener('click',function(){
+    const list = document.getElementById('list');
+    const name = document.getElementById('karim');
+    const realName = name.innerText;
+    const li = document.createElement('li')
+    li.innerText = realName;
+    list.appendChild(li);
+    
+})
+document.getElementById('karim-btn').addEventListener('click',function(){
+    const list = document.getElementById('list');
+    const name = document.getElementById('karim');
+    const realName = name.innerText;
+    const li = document.createElement('li')
+    li.innerText = realName;
+    list.appendChild(li);
+    
+})
+document.getElementById('karim-btn').addEventListener('click',function(){
+    const list = document.getElementById('list');
+    const name = document.getElementById('karim');
     const realName = name.innerText;
     const li = document.createElement('li')
     li.innerText = realName;
