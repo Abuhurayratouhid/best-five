73d7b9eb9a93886cac232c82e99def26fd82eb32
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-08-22 18:14:31 -0700

    eventhandler added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('knock from js file')
+// console.log('knock from js file')
+
+document.getElementById('calculate').addEventListener('click',function(){
+    // get perPlayerInput 
+    const perPlayerInput = document.getElementById('per-player-input');
+    const playerInputString = perPlayerInput.value ;
+    const playerInputAmount = parseFloat(playerInputString)
+
+    // get player-expenses 
+    const playerExpensesField = document.getElementById('player-expenses');
+    const expensesFieldString = playerExpensesField.innerText;
+    const expensesFieldAmount = parseFloat(expensesFieldString);
+    console.log(expensesFieldAmount)
+})
+
+document.getElementById('calculate-total-btn').addEventListener('click',function(){
+    console.log('total btn clicked')
+})
