656bcb88911c27411f8d2fcffebb76e31730021c
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-08-22 19:00:44 -0700

    add another js file

--- js/app.js
+++ js/app.js
@@ -10,9 +10,26 @@ document.getElementById('calculate').addEventListener('click',function(){
     const playerExpensesField = document.getElementById('player-expenses');
     const expensesFieldString = playerExpensesField.innerText;
     const expensesFieldAmount = parseFloat(expensesFieldString);
-    console.log(expensesFieldAmount)
+    // console.log(expensesFieldAmount)
 })
 
+
+
+
 document.getElementById('calculate-total-btn').addEventListener('click',function(){
-    console.log('total btn clicked')
+    // get player-expenses
+    const playerExpensesField = document.getElementById('player-expenses');
+    const expensesFieldString = playerExpensesField.innerText;
+    const expensesFieldAmount = parseFloat(expensesFieldString);
+    
+    // get manager input field 
+    const managerInputField = document.getElementById('manager-input');
+    const managerInputFieldString = managerInputField.value ;
+    const managerInputFieldAmount = parseFloat(managerInputFieldString);
+
+    // get coachfield 
+    const coachfieldInput = document.getElementById('coachfee-input');
+    const coachfieldString = coachfieldInput.innerText;
+    const coachfieldAmount = parseFloat(coachfieldString);
+    console.log(coachfieldAmount)
 })
---
+++ js/select.js
@@ -0,0 +1,1 @@
+console.log('from select .js')
