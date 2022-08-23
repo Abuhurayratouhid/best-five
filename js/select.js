09d82ffd904c093895fdcf3beb08141e45200d79
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-08-22 19:59:16 -0700

    some important comment added

--- js/select.js
+++ js/select.js
@@ -1,1 +1,10 @@
-console.log('from select .js')
+// console.log('from select .js')
+document.getElementById('ozil-btn').addEventListener('click',function(){
+    const list = document.getElementById('list');
+    const name = document.getElementById('ozil');
+    const realName = name.innerText;
+    const li = document.createElement('li')
+    li.innerText = realName;
+    list.appendChild(li);
+    
+})
